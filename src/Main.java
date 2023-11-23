18a9f6623215065f5ee045dc00de7feb30929667
LVVSys <Driver-DIch@mail.ru>
2023-11-23 03:37:00 +0300

    Begin

--- src/Main.java
+++ src/Main.java
@@ -13,5 +13,23 @@ public class Main {
         System.out.println("Значение переменной d с типом long равно " + d);
         System.out.println("Значение переменной e с типом float равно " + e);
         System.out.println("Значение переменной f с типом double равно " + f);
+
+        float firstNumber = 27.12f;
+        long secondNumber = 987678965549L;
+        double thirdNMumber = 2.786;
+        int fourthNumber = 569;
+        short fifthNumber = -159;
+        int sixthNumber = 27897;
+        byte seventhNumber = 67;
+        System.out.println(firstNumber);
+        System.out.println(secondNumber);
+        System.out.println(thirdNMumber);
+        System.out.println(fourthNumber);
+        System.out.println(fifthNumber);
+        System.out.println(sixthNumber);
+        System.out.println(seventhNumber);
+
+
+
     }
 }
