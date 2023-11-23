141d9eb2e21d627882452db95acd1e9473fb52ac
LVVSys <Driver-DIch@mail.ru>
2023-11-23 03:13:15 +0300

    Begin

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,17 @@
 public class Main {
     public static void main(String[] args) {
 
-        System.out.println("Hello world!");
+        byte a = 10;
+        short b = 27000;
+        int c = 55555;
+        long d = 23456789L;
+        float e = 2.73f;
+        double f = 3.748;
+        System.out.println("Значение переменной a с типом byte равно " + a);
+        System.out.println("Значение переменной b с типом short равно " + b);
+        System.out.println("Значение переменной c с типом int равно " + c);
+        System.out.println("Значение переменной d с типом long равно " + d);
+        System.out.println("Значение переменной e с типом float равно " + e);
+        System.out.println("Значение переменной f с типом double равно " + f);
     }
 }
